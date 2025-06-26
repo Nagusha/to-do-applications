d3ae69f7cf6d38b5485526822eb45e87f2ea114d
nagusha <nagusha.madasu@everest.engineering>
2025-06-26 13:41:47 +0530

    Update the active records in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_26_015611) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_26_023010) do
   create_table "to_dos", force: :cascade do |t|
     t.string "title"
     t.boolean "completed"
