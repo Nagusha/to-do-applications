12fbf34916c7f701a7d517bc10f4f50375a80953
nagusha <nagusha.madasu@everest.engineering>
2025-06-26 11:27:23 +0530

    Add a login, logout, signup and users on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  root "to_dos#index"
+   root "sessions#new"
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  get "/signup", to: "users#new"
+  post "/users", to: "users#create"
 
   resources :to_dos, only: [ :index, :create, :destroy ] do
     member do
